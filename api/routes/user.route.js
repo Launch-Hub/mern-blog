4df866fb2413a19322ed4d154af78148e2fdac1b
GiangNT <76811950+giangntse150746@users.noreply.github.com>
2025-01-02 01:32:55 +0700

    Add authentication endpoints
    Add authentication endpoints for blog

---
+++ api/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+
+const router = express.Router();
+
+router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+
+export default router;
